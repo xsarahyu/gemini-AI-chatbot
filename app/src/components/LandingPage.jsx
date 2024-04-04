285a59136bde22abe7ee0e83ec2a16a1119daf3e
Sarah <xsarahyu@gmail.com>
2024-04-04 19:08:45 -0400

    Style LandingPage and NavBar

--- app/src/components/LandingPage.jsx
+++ app/src/components/LandingPage.jsx
@@ -1,11 +1,9 @@
 const LandingPage = () => {
-    return (
-      <div>
-        <h1>Welcome to the Landing Page!</h1>
-        {/* Additional content here */}
-      </div>
-    );
-  }
-  
-  export default LandingPage;
-  
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 p-10">
+      <h1 className="text-5xl font-bold text-gray-800 mb-8">Welcome to the Resilient Coders AI ChatBot!</h1>
+    </div>
+  )
+}
+
+export default LandingPage
--- app/src/components/NavBar.jsx
+++ app/src/components/NavBar.jsx
@@ -2,10 +2,14 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
+    <nav className="bg-rc-red py-4">
+      <ul className="flex justify-center">
+        <li className="mx-4">
+          <Link to="/" className="text-white text-2xl hover:text-gray-300">Home</Link>
+        </li>
+        <li className="mx-4">
+          <Link to="/login" className="text-white text-2xl hover:text-gray-300">Login</Link>
+        </li>
       </ul>
     </nav>
   );
