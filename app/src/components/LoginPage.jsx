e959bc7b6c5520b30655d39ec7fa53c79109fe0e
Jasmine Moore <dev.jasminedm@gmail.com>
2024-04-06 20:46:19 -0400

    Adding a tailwind componenet to style the Login form including some added css to index.css

--- app/src/components/LoginPage.jsx
+++ app/src/components/LoginPage.jsx
@@ -20,35 +20,44 @@ const LoginPage = () => {
 
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Username:
-          <input 
-          value={loginData.userName} 
-          type='text'
-          name='userName'
-          onChange={handleChange}
-          />
-        </label>
-
-        
-        <label>
-          Password:
-          <input 
-          value={loginData.password} 
-          type='password'
-          name='password'
-          onChange={handleChange}
-          />
-        </label>
-
-        <button type='submit'>Login</button>
-      </form>
-      
-      
+    <div class="flex justify-center items-center h-screen">
+<div class="w-full max-w-xs">
+  <form onSubmit={handleLogin} class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+      id="username" 
+      placeholder="Username"
+      value={loginData.userName} 
+      type='text'
+      name='userName'
+      onChange={handleChange}></input>
     </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+      id="password" 
+      placeholder="******************"
+      value={loginData.password} 
+      type='password'
+      name='password'
+      onChange={handleChange}></input>
+      <p class="text-red-500 text-xs italic">Please choose a password.</p>
+    </div>
+    <div class="flex items-center justify-between">
+      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+        Sign In
+      </button>
+    </div>
+  </form>
+</div>
+</div>
+
+    
   );
 };
 
