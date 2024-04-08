2d8961441f9187f914eaf565122fca89f230741f
cintnguyen <cintnguyen98@gmail.com>
2024-04-08 15:19:24 -0400

    message component testing

--- app/src/components/MessagesComponent.test.js
+++ app/src/components/MessagesComponent.test.js
@@ -21,8 +21,3 @@ describe('Messages component', () => {
         expect(messageContent).toBeInTheDocument()
     })
 })
-
-// use npm test
-// Mock using a test message in the input
-// Mock submitting the message
-// Check for what is expected, expected the content is rendered to the DOM
