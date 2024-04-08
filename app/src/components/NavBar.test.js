045aa433833a4d5b839bdc006ae99a9d365b1831
cintnguyen <cintnguyen98@gmail.com>
2024-04-08 17:59:06 -0400

    nav bar component testing

---
+++ app/src/components/NavBar.test.js
@@ -0,0 +1,30 @@
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { BrowserRouter } from 'react-router-dom'
+import NavBar from './NavBar'
+
+describe('Nav bar component', () => {
+    test('renders Nav Bar ', () => {
+        const { getByTestId } = render(
+            <BrowserRouter>
+                <NavBar />
+            </BrowserRouter>
+        );
+
+        const homeLink = getByTestId('homeNavBarButton');
+        expect(homeLink).toBeInTheDocument();
+
+        const loginLink = getByTestId('loginNavBarButton');
+        expect(loginLink).toBeInTheDocument();
+    }),
+    test('renders Nav Bar ', () => {
+        const { getByTestId } = render(
+            <BrowserRouter>
+                <NavBar />
+            </BrowserRouter>
+        );
+
+        const loginLink = getByTestId('loginNavBarButton');
+        expect(loginLink).toBeInTheDocument();
+    })
+})
